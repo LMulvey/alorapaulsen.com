076d1dc1f3106cae3535eff2d1b36bb6e05e08d2
Lee Mulvey <lmulvey@me.com>
2021-05-10 14:47:37 -0600

    fix: font paths

--- src/styles/fonts.css.ts
+++ src/styles/fonts.css.ts
@@ -1,35 +1,35 @@
 import { globalFontFace } from '@vanilla-extract/css';
 
 globalFontFace('Quicksand', {
-  src: 'url(fonts/Quicksand-Light.ttf)',
+  src: 'url(/fonts/Quicksand-Light.ttf)',
   fontStyle: 'normal',
   fontWeight: 300,
   fontDisplay: 'swap',
 });
 
 globalFontFace('Quicksand', {
-  src: 'url(fonts/Quicksand-Regular.ttf)',
+  src: 'url(/fonts/Quicksand-Regular.ttf)',
   fontStyle: 'normal',
   fontWeight: 400,
   fontDisplay: 'swap',
 });
 
 globalFontFace('Quicksand', {
-  src: 'url(fonts/Quicksand-Medium.ttf)',
+  src: 'url(/fonts/Quicksand-Medium.ttf)',
   fontStyle: 'normal',
   fontWeight: 500,
   fontDisplay: 'swap',
 });
 
 globalFontFace('Quicksand', {
-  src: 'url(fonts/Quicksand-SemiBold.ttf)',
+  src: 'url(/fonts/Quicksand-SemiBold.ttf)',
   fontStyle: 'normal',
   fontWeight: 600,
   fontDisplay: 'swap',
 });
 
 globalFontFace('Quicksand', {
-  src: 'url(fonts/Quicksand-Bold.ttf)',
+  src: 'url(/fonts/Quicksand-Bold.ttf)',
   fontStyle: 'normal',
   fontWeight: 700,
   fontDisplay: 'swap',
